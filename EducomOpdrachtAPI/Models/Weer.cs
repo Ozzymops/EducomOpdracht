2a3c40f7131e45211ff7bab8356cd1a07b8a8195
Ozzymops <justinmuris@gmail.com>
2021-06-14 14:42:00 +0200

    Modellen opgezet.

---
+++ EducomOpdrachtAPI/Models/Weer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducomOpdrachtAPI.Models
+{
+    public class Weer
+    {
+        /// <summary>
+        /// Een entry in de database.
+        /// Opgeslagen data bestaat uit datum, temperatuur, luchtvochtigheid en luchtdruk.
+        /// </summary>
+        public long StationId { get; set; }
+        public DateTime Date { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int AirPressure { get; set; }
+    }
+}
---
+++ EducomOpdrachtAPI/Models/Weerstation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducomOpdrachtAPI.Models
+{
+    public class Weerstation
+    {
+        /// <summary>
+        /// Een entry in de database.
+        /// Weerstation vanuit https://data.buienradar.nl/1.1/feed/json opgehaald. Opgeslagen data bestaat uit station id, stationregio en stationnaam.
+        /// Wordt gebruikt voor een dropdown menu in de andere applicatie.
+        /// </summary>
+        public long Id { get; set; }
+        public string Region { get; set; }
+        public string Name { get; set; }
+    }
+}
