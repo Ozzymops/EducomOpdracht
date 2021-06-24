65e1f5f6068e84596b75bca75ad87b2393868150
Ozzymops <justinmuris@gmail.com>
2021-06-24 20:08:40 +0200

    Documentatie EducomOpdrachtTaskScheduler

--- EducomOpdrachtTaskScheduler/Models/Weerbericht.cs
+++ EducomOpdrachtTaskScheduler/Models/Weerbericht.cs
@@ -8,17 +8,28 @@ namespace EducomOpdrachtTaskScheduler.Models
 {
     class Weerbericht
     {
-        /// <summary>
-        /// Een entry in de database. Toekomstige data uit 5-daags weervoorspelling.
-        /// </summary>
+        // ID in de database
         public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public int MaxTemperature { get; set; }     // maximum temperatuur in graden Celsius
-        public int MinTemperature { get; set; }     // minimum temperatuur in graden Celsius
-        public int Windspeed { get; set; }          // windsnelheid volgens de schaal van Beaufort: https://www.knmi.nl/kennis-en-datacentrum/uitleg/windschaal-van-beaufort
-        public int RainChance { get; set; }         // kans op regen in %
-        public int SunChance { get; set; }          // kans op zon in %
 
+        // Datum van weerbericht
+        public DateTime Date { get; set; }
+
+        // Maximum temperatuur van de dag in graden Celsius
+        public int MaxTemperature { get; set; }
+
+        // Minimum temperatuur van de dag in graden Celsius
+        public int MinTemperature { get; set; }
+
+        // Windsnelheid gedurende de dag volgens de schaal van Beaufort: https://www.knmi.nl/kennis-en-datacentrum/uitleg/windschaal-van-beaufort
+        public int Windspeed { get; set; }
+
+        // Kans op regen gedurende de dag in percentage
+        public int RainChance { get; set; }
+
+        // Kans op zon gedurende de dag in percentage
+        public int SunChance { get; set; }
+
+        // Constructor voor gemak
         public Weerbericht(DateTime date, int maxTemp, int minTemp, int windspeed, int rainChance, int sunChance)
         {
             this.Date = date;
--- EducomOpdrachtTaskScheduler/Program.cs
+++ EducomOpdrachtTaskScheduler/Program.cs
@@ -15,7 +15,6 @@ namespace EducomOpdrachtTaskScheduler
     {
         static void Main(string[] args)
         {
-            // API URL om uit te voeren, in dit geval de update database functie.
             Console.WriteLine(DateTime.Now.ToString() + " | Console application started.");
             UpdateDatabase();
             Console.WriteLine(DateTime.Now.ToString() + " | Finished, console will close in 5 seconds...");
@@ -24,7 +23,6 @@ namespace EducomOpdrachtTaskScheduler
 
         public static void UpdateDatabase()
         {
-            // Weerstations
             if (DoPost(0))
             {
                 Console.WriteLine(DateTime.Now.ToString() + " | Weatherstation operation succesful.");
@@ -34,7 +32,6 @@ namespace EducomOpdrachtTaskScheduler
                 Console.WriteLine(DateTime.Now.ToString() + " | Weatherstation operation aborted.");
             }
 
-            // Weerberichten
             if (DoPost(1))
             {
                 Console.WriteLine(DateTime.Now.ToString() + " | Weather forecast operation succesful.");
@@ -51,6 +48,7 @@ namespace EducomOpdrachtTaskScheduler
             {
                 string url = string.Empty;
 
+                // Bepaal URL uit gegeven type
                 if (type == 0)
                 {
                     url = System.Configuration.ConfigurationManager.AppSettings["weerstationsUrl"];
@@ -60,14 +58,14 @@ namespace EducomOpdrachtTaskScheduler
                     url = System.Configuration.ConfigurationManager.AppSettings["weerberichtenUrl"];
                 }
 
+                // Creëer een web request (GET) om de API te triggeren (GetAndPostWeerstationList & GetAndPostWeerberichtList)
                 HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
                 webRequest.Method = "GET";
                 webRequest.ContentType = "application/x-www-form-urlencoded";
                 
-                // Authorisatie voor toegang tot POST methode in API
+                // Authorisatie voor toegang in API
                 string encodedString = System.Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(ConfigurationManager.AppSettings["consoleUsername"].ToString() + ":" + ConfigurationManager.AppSettings["consolePassword"].ToString()));
                 webRequest.Headers.Add("Authorization", "Basic " + encodedString);
-
 
                 Console.WriteLine(DateTime.Now.ToString() + " | Done. Executing web response...");
                 HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
